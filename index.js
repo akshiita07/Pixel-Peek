56173fa9a744dc3bba6cc09932649abc40852ae4
Akshita Pathak <pathakshita07@gmail.com>
2025-04-05 14:34:42 +0530

    cursor changes with explanations added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-﻿window.addEventListener('DOMContentLoaded', function () {
+﻿// run code only after all html elements are loaded
+window.addEventListener('DOMContentLoaded', function () {
     // Extract canvas using its id
     var canvas = document.getElementById('myCanvas');
-    // expecting a 2D context:
+    // use a 2D context to draw image on this canvas:
     var context = canvas.getContext('2d');
-    // add image to canvas:
+
+    // create an image object & add image to canvas:
     var image = new Image();
     image.src = "logo.png";
 
@@ -19,17 +21,22 @@
 
     // get pixel color on moving over canvas
     canvas.addEventListener('mousemove', function (e) {
-        const rect = canvas.getBoundingClientRect();
+        const rect = canvas.getBoundingClientRect(); //to get canvas position & size
+        // caluculate position of mouse relative to canvas
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
 
+        // use getImageData to retrive color of pixel at that mouse location
         const pixel = context.getImageData(x, y, 1, 1).data;
+        // data array will contain rgb color values at that pixel
         const r = pixel[0];
         const g = pixel[1];
         const b = pixel[2];
 
+        // use a fnc to convert rgb color code to hex color
         const hex = rgbToHex(r, g, b);
 
+        // print it in the div
         showColor.innerHTML = `RGB: (${r}, ${g}, ${b}) <br> HEX: ${hex}`;
         showColor.style.backgroundColor = hex;
     });
